4650cbf52e0c8ad6d216589936a4e5978addf6c1
David Farias <davidfarias30@gmail.com>
2023-04-27 14:23:26 -0400

    addVideo layout created

---
+++ src/components/AddVideo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="add-video">
+        <h1>
+            Añadir nuevo video
+        </h1>
+        <div class="inputs-container">
+            <input type="text">
+            <v-btn color="blue" width="253" height="50">Añadir</v-btn>
+        </div>
+    </div>
+</template>
+<style scoped>
+.add-video {
+    width: 80%;
+}
+.inputs-container{
+    display: flex;
+}
+h1 {
+    margin-bottom: 20px;
+}
+input {
+    height: 50px;
+    width: 100%;
+    border: 1px solid rgb(133, 133, 133);
+    border-radius: 5px 0px 0px 5px;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,8 @@
+<script setup>
+import AddVideo from '../components/AddVideo.vue'
+</script>
+<template>
+    <div>
+        <AddVideo />
+    </div>
+</template>
